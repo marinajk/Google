fcc311b3ce2206f6f82bad3243ef545c05ac2a69
Marina John <marinajohnkennedy@gmail.com>
2019-01-22 16:47:18 +0530

    Browser tab icon done.

---
+++ myscript.js
@@ -0,0 +1,13 @@
+   <script>
+       
+    function getfocustxt()
+    {
+ 
+        search=document.getElementById("search").style;
+        search.borderstyle="none";
+        search.borderColor="white";
+      document.getElementById("search").focus();
+
+    }
+    window.onload=getfocustxt;
+</script>
